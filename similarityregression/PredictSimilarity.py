1e407c01e5be5b608b6f646562a544e118be58cc
smlmbrt <sam.a.lambert@gmail.com>
2018-06-25 15:29:13 -0400

    Updated scoring script to handle positons where sd=0 and use results from the pairwise alignment script

--- similarityregression/PredictSimilarity.py
+++ similarityregression/PredictSimilarity.py
@@ -7,34 +7,44 @@ logistic = lambda x: 1 / (1 + np.exp(-x))
 def ReadSRModel(filename):
     with open(filename) as SRModel:
         srmodel = json.load(SRModel)
+    #Convert to NP arrarys
     if 'SR.Weights' in srmodel:
         srmodel['SR.Weights'] = np.asarray(srmodel['SR.Weights'])
+        srmodel['SR.FeatureScales.mean'] = np.asarray(srmodel['SR.FeatureScales.mean'])
+        #Convert 0's to NAs
+        sd = np.asarray(srmodel['SR.FeatureScales.sd'])
+        sd[sd == 0] = np.nan
+        srmodel['SR.FeatureScales.sd'] = sd
+    #Check for Amb/Dis threshold
+    if np.isnan(srmodel['Threshold.Dis']):
+        srmodel['Threshold.Dis'] = None
     return(srmodel)
     
 def ScoreAlignmentResult(resultDict, scoreDict):
     #Score The Sequence
-    if scoreDict['ModelType'] == 'SequenceIdentity':
+    if scoreDict['Model.Class'] == 'SequenceIdentity':
         Score = resultDict[scoreDict['ModelName']]
     else:
-        weights = scoreDict['Weights']
-        if scoreDict['ByPos'] == 'PctID':
-            ByPos = resultDict['ByPos_PctID']
-        else:
-            ByPos = resultDict['ByPos_AvgB62']
-            
-        Score = scoreDict['Intercept'] + np.dot(weights, ByPos)
-        
-    #Check if it should be transformed re: logistic
-    if scoreDict['ModelType'] == 'Classifier':
-        Score = logistic(Score)
+        SRweights = scoreDict['SR.Weights']
+        #Get postional scores
+        ByPos = np.array(resultDict['ByPos.' + scoreDict['SR.Features'].replace('_','.')])
+        #Normalize to features (f)
+        f = (ByPos - scoreDict['SR.FeatureScales.mean'])/scoreDict['SR.FeatureScales.sd']
+        f[np.isnan(f)] = 0 #Cleanup NAs
+        Score = scoreDict['SR.Intercept'] + np.dot(SRweights, f)
+        if scoreDict['SR.LogisticTransform'] == True:
+            logistic = lambda x: 1 / (1 + np.exp(-x))
+            Score = logistic(Score)
         
     #Classify Score Based on Thresholds
-    if Score >= scoreDict['Threshold_0.75']:
+    ##Check if HSim/Amb
+    Classification = np.nan
+    if Score >= scoreDict['Threshold.HSim']:
         Classification = 'HSim'
-    elif Score >= scoreDict['Threshold_0.25']:
-        Classification = 'Amb'
-    elif Score < scoreDict['Threshold_0.25']:
-        Classification = 'Dis'
     else:
-        Classification = 'NA'
+        Classification = 'Amb'
+    ##Check if Amb/Dis        
+    if scoreDict['Threshold.Dis'] != None:
+        if Score < scoreDict['Threshold.Dis']:
+            Classification = 'Dis'
     return(Score, Classification)
