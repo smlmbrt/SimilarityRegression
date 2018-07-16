fad325a4541547199d140477d88bfb027eb82a9b
smlmbrt <sam.a.lambert@gmail.com>
2018-07-16 17:06:38 -0400

    Added iterators and functions to make and parrellize CIS-BP inferences

--- similarityregression/PredictSimilarity.py
+++ similarityregression/PredictSimilarity.py
@@ -52,3 +52,62 @@ def ScoreAlignmentResult(resultDict, scoreDict, applyidenticalRule = True):
         Classification = 'HSim'
     
     return(Score, Classification)
+
+#Functions to iterate over Sequence Dictionaries seq : [(TF_ID, M_ID), ...] or [(TF_ID, P_ID), ...]
+def SeqDictIterator_XoverY(mdict, pdict):
+    for seq_m, ids_m in mdict.items():
+        seq_m = seq_m.split(',')
+        ids_m.sort()
+        m = (ids_m, seq_m)
+        for seq_p, ids_p in pdict.items():
+            seq_p = seq_p.split(',')
+            ids_p.sort()
+            p = (ids_p, seq_p)
+            yield(m, p)
+
+def SeqDictIterator_XoverX(pdict):
+    for seq_i, seq_j in itertools.combinations(pdict.keys(),2):
+        ids_i = pdict[seq_i]
+        ids_j = pdict[seq_j]
+        i = (ids_i, seq_i.split(','))
+        j = (ids_j, seq_j.split(','))
+        yield(i, j)
+        
+#Function to map
+from similarityregression import PairwiseAlignment as pwsaln
+def AlignAndScore_DictPairs(t, OutputClasses = ['HSim', 'Dis']):
+    OutputClasses = set(OutputClasses)
+    #Unpack input
+    i_ids = t[0][0]
+    i_seq = t[0][1]
+    
+    j_ids = t[1][0]
+    j_seq = t[1][1]
+    
+    #Align Sequences
+    aln_result = pwsaln.AlignDBDArrays(('i', i_seq), ('j', j_seq))
+    
+    #Score alignment
+    aln_score = srpred.ScoreAlignmentResult(resultDict=aln_result, scoreDict=SRModel)
+    aln_Class = aln_score[1]
+    
+    #Parse 2 results list
+    results = []
+    if aln_Class in OutputClasses:
+        for i in i_ids:
+            for j in j_ids:
+                if i < j:
+                    results.append(list(i) + list(j) + list(aln_score))
+                else:
+                    results.append(list(j) + list(i) + list(aln_score))
+    return(aln_Class, results)
+
+def SeqDictIterator_ParseIdentical2Results(d):
+    results = []
+    for ids in d.values():
+        for i, j in itertools.combinations(ids, 2):
+            if i < j:
+                results.append(list(i) + list(j) + [1, 'HSim'])
+            else:
+                results.append(list(j) + list(i) + [1, 'HSim'])
+    return(results)
